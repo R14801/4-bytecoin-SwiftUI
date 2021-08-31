0a3f49b80864228798d64d76e66faaf5bda9a340
Rajat Nagvenkar <rajatnagvenker@gmail.com>
2021-08-31 22:23:01 +0530

    Initial Commit

---
+++ ByteCoin 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ByteCoin 2
+//
+//  Created by Rajat Nagvenker on 8/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
